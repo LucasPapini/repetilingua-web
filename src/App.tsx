a312d06448cfd37271b5399adc7fd6d01e28904e
LucasPapini <papine12345@hotmail.com>
2026-09-08 18:37:03 -0300

    initial(repetilingua): configurar o Path Alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/Button'
 import './styles.css'
 
 function App() {
   return (
     <>
       <h1>Hellor world</h1>
+      <Button title="Teste" />
     </>
   )
 }

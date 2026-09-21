b6e7bfd29aa6536bed9ec265a45e7cf0944847e5
LucasPapini <papine12345@hotmail.com>
2026-09-21 12:27:44 -0300

    hotfix: Ajuste para retirar erro do baseUrl descontinuado. -Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,31 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Repetilingua',
+      short_name: 'Repetilingua',
+      description: 'App para treino de repetição de inglês',
+      theme_color: '#000a1e',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

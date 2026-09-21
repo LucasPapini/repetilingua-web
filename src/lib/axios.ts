fa1a30c4ea1c44f2a9280d5c99127e64eba21b5b
LucasPapini <papine12345@hotmail.com>
2026-09-21 20:44:56 -0300

    fix: ajuste interceptors v2.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,19 +3,18 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
+    'ngrok-skip-browser-warning': 'true',
     'Content-Type': 'application/json',
   },
 });
 
 // Interceptador opcional para injetar o Token JWT
 api.interceptors.request.use((config) => {
-  // Corrigido para incluir a letra "i" em repetilingua
   const token = localStorage.getItem('@repetilingua:access_token');
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-    config.headers['ngrok-skip-browser-warning'] = 'true';
   }
-
+  // Asegura el envío del encabezado de ngrok en cada solicitud
+  config.headers['ngrok-skip-browser-warning'] = 'true';
   return config;
 });

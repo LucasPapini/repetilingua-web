3c549d0bf99dd62360e28de8c540b2faf7b07a91
LucasPapini <papine12345@hotmail.com>
2026-09-21 21:27:14 -0300

    fix: fixando a url padrao do servidor.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://4c59-2804-7f0-b77d-e9-8657-c20d-5063-c87.ngrok-free.app',
   headers: {
     'ngrok-skip-browser-warning': 'true',
     'Content-Type': 'application/json',

6a046b921d4596077f089cf7481121c1a629e9b1
LucasPapini <papine12345@hotmail.com>
2026-09-21 21:52:32 -0300

    fix: escrevendo errado o nome do token que vai ficar no localstorage

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -10,7 +10,7 @@ export const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('@repetilingua:access_token');
+    const token = localStorage.getItem('@repetilngua:access_token');
 
     if (token && config.headers) {
       // Garante a definição do cabeçalho Bearer
